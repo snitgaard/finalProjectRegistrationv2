e9218743c34b1e2d9667e222726ff46a38cb0fe7
dreamy <31621059+dreamyy1@users.noreply.github.com>
2020-11-05 09:59:28 +0100

    Initial commit

---
+++ Interfaces/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface IStudentRepository
+    {
+        IEnumerable<Student> GetAll();
+        Student GetById(int id);
+        void Add(Student s);
+        void Update(Student s);
+        void Remove(Student s);
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string PostalDistrict { get; set; }
+        public string Email { get; set; }
+    }
+}
